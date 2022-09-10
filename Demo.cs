1fea25a7fc1d690233430ec0f9903794101bf9b1
tony <anm@umbraco.dk>
2022-09-10 02:43:28 +0200

    security issue fixed

--- Demo.cs
+++ Demo.cs
@@ -2,6 +2,6 @@
 {
 	public static class Demo
 	{
-		public static string Concat(string a, string b) => $"{a}{b}SECURITY ISSUE";
+		public static string Concat(string a, string b) => $"{a}{b}";
 	}
 }
